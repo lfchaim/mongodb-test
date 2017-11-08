520c91bd3d7637218a070fd86cd707b20f950af3
fernando <fernando@CCE>
2017-11-08 15:50:14 -0200

    Implementacao

--- src/main/java/br/com/whs/mongodbtest/TestMongo.java
+++ src/main/java/br/com/whs/mongodbtest/TestMongo.java
@@ -4,24 +4,59 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.bson.Document;
+import org.bson.types.ObjectId;
 
-import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.result.DeleteResult;
 
 import br.com.whs.mongodbtest.dbutil.DBUtil;
 
 public class TestMongo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		MongoClient mc = DBUtil.getMongoClient();
-		MongoDatabase db = mc.getDatabase("escola");
-		MongoCollection<Document> professores = db.getCollection("professores");
-		List<Document> list = professores.find().into(new ArrayList<Document>());
+		//insertTest();
+		deleteTest();
+		listTest();
+	}
+
+	private static void listTest() {
+		List<Document> list = DBUtil.listCollection("escola", "professores");
 		for( Document doc : list ) {
 			System.out.println(doc.toJson());
 		}
 	}
-
+	
+	private static void insertTest() {
+		Document docDetail = new Document();
+		docDetail.put("name", "Jason");
+		docDetail.put("idade", 47);
+		List<String> disc = new ArrayList<String>();
+		disc.add("Matemática");
+		disc.add("Estatística");
+		docDetail.put("disciplinas", disc);
+		docDetail.put("sexo", "m");
+		DBUtil.insertOne("escola", "professores", docDetail);
+	}
+	
+	private static void deleteTest() {
+		try {
+			MongoCollection<Document> mc = DBUtil.getMongoCollection("escola", "professores");
+			FindIterable<Document> fi = mc.find(Filters.eq("_id", new ObjectId("5a031b7c36384a5ca8525dce")));
+			MongoCursor<Document> curs = fi.iterator();
+			while( curs.hasNext() ) {
+				Document doc = curs.next();
+				System.out.println(doc);
+				if( doc.getObjectId("_id").equals(new ObjectId("5a031b3736384a5c569bde8d")) )
+					mc.deleteOne(doc);
+					
+			}
+			//DeleteResult dr = mc.deleteOne(new Document("_id","5a031b3736384a5c569bde8d"));
+			//System.out.println(dr);
+		}catch( Exception e ) {
+			e.printStackTrace();
+		}
+	}
 }
--- src/main/java/br/com/whs/mongodbtest/dbutil/DBUtil.java
+++ src/main/java/br/com/whs/mongodbtest/dbutil/DBUtil.java
@@ -1,12 +1,60 @@
 package br.com.whs.mongodbtest.dbutil;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bson.Document;
+
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 public class DBUtil {
 
 	public static MongoClient getMongoClient() {
 		MongoClient mc = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
 		return mc;
+	}
+	
+	public static List<Document> listCollection( String dbName, String collectName ){
+		MongoClient mc = getMongoClient();
+		MongoDatabase db = mc.getDatabase(dbName);
+		MongoCollection<Document> collDoc = db.getCollection(collectName);
+		List<Document> list = collDoc.find().into(new ArrayList<Document>());
+		return list;
+	}
+	
+	public static MongoCollection<Document> getMongoCollection( String dbName, String collectName ){
+		MongoClient mc = getMongoClient();
+		MongoDatabase db = mc.getDatabase(dbName);
+		MongoCollection<Document> collDoc = db.getCollection(collectName);
+		return collDoc;
+	}
+	
+	public static boolean insertOne( String dbName, String collectName, Document document ) {
+		try {
+			MongoClient mc = getMongoClient();
+			MongoDatabase db = mc.getDatabase(dbName);
+			MongoCollection<Document> collDoc = db.getCollection(collectName);
+			collDoc.insertOne(document);
+		}catch( Exception e ) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	
+	public static boolean deleteOne( String dbName, String collectName, Document document ) {
+		try {
+			MongoClient mc = getMongoClient();
+			MongoDatabase db = mc.getDatabase(dbName);
+			MongoCollection<Document> collDoc = db.getCollection(collectName);
+			collDoc.deleteOne(document);
+		}catch( Exception e ) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
 	}
 }
